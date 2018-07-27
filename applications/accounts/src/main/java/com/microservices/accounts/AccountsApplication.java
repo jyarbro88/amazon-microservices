92c66b83c9025e723b6b39861a838a8b5314d291
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-27 15:32:03 -0500

    removed Lombok because it was causing problems.  i dont know why

--- applications/accounts/src/main/java/com/microservices/accounts/AccountsApplication.java
+++ applications/accounts/src/main/java/com/microservices/accounts/AccountsApplication.java
@@ -1,12 +1,31 @@
 package com.microservices.accounts;
 
+import com.microservices.accounts.accounts.Account;
+import com.microservices.accounts.accounts.AccountRepository;
+import com.microservices.accounts.addresses.Address;
+import com.microservices.accounts.addresses.AddressRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccountsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner populateDatabase(AccountRepository accountRepository, AddressRepository addressRepository) {
+
+		return (args) -> {
+		    Account account = new Account("joe", "yarbrough", "email@email.com", null);
+
+		    Address address = new Address("811 E Stone ct", "apt", "Addison", "IL", "60101", "USA");
+
+		    accountRepository.save(account);
+		    addressRepository.save(address);
+        };
 	}
 }
--- applications/accounts/src/main/java/com/microservices/accounts/accounts/AccountController.java
+++ applications/accounts/src/main/java/com/microservices/accounts/accounts/AccountController.java
@@ -1,9 +1,37 @@
 package com.microservices.accounts.accounts;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
 public class AccountController {
+
+//    @Autowired
+//    private AccountRepository accountRepository;
+//
+//    @GetMapping(produces = "application/json")
+//    public String sayHello(){
+//        return "hello";
+//    }
+//
+//    @GetMapping(value = "/all")
+//    @ResponseBody
+//    public List<Account> showAllAccounts(){
+//        return accountRepository.findAll();
+//    }
+//
+//    @GetMapping(value = "/{accountId}", produces = "application/json")
+//    public Optional<Account> searchById(
+//            @Valid
+//            @PathVariable(value = "accountId") Long accountId
+//    ){
+//
+//        return accountRepository.findById(accountId);
+//    }
 
 }
