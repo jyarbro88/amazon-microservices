e71e4f6bbd5ac35ace2ef571b621214a0d56bf19
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-27 11:04:35 -0500

    added in products project directory

--- applications/orders/src/main/java/com/microservices/orders/LineItems/LineItemController.java
+++ applications/orders/src/main/java/com/microservices/orders/LineItems/LineItemController.java
@@ -1,4 +1,15 @@
 package com.microservices.orders.LineItems;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = {"/lineItems"})
 public class LineItemController {
+
+    @GetMapping
+    public String sayHello(){
+        return "hello from line items";
+    }
 }
