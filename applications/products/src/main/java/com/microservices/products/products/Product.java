f4943ee9f8af5273404945f9be0ad03ba53c6cd2
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-29 12:48:21 -0500

    updated class and Lombok

--- applications/products/src/main/java/com/microservices/products/products/Product.java
+++ applications/products/src/main/java/com/microservices/products/products/Product.java
@@ -10,8 +10,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Getter @Setter @NoArgsConstructor
 @Table(name = "products")
-public class Product {
+class Product {
 
     @Id
     @GeneratedValue
@@ -20,38 +21,4 @@ public class Product {
     private String description;
     private Double price;
 
-    public Product() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
