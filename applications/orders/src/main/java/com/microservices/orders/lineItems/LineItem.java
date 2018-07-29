6e8e02721ce0dc48c8b6060e1599c542bc8b8914
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-29 12:46:54 -0500

    added Lomback back to model

--- applications/orders/src/main/java/com/microservices/orders/lineItems/LineItem.java
+++ applications/orders/src/main/java/com/microservices/orders/lineItems/LineItem.java
@@ -1,12 +1,17 @@
 package com.microservices.orders.lineItems;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "order_line_items")
+@Getter @Setter @NoArgsConstructor
+@Table(name = "line_items")
 public class LineItem {
 
     @Id
@@ -21,78 +26,4 @@ public class LineItem {
     private Boolean shipped;
     private Boolean delivered;
 
-    public LineItem() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public Long getShipmentId() {
-        return shipmentId;
-    }
-
-    public void setShipmentId(Long shipmentId) {
-        this.shipmentId = shipmentId;
-    }
-
-    public Long getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Long orderId) {
-        this.orderId = orderId;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Double getSingleItemPrice() {
-        return singleItemPrice;
-    }
-
-    public void setSingleItemPrice(Double singleItemPrice) {
-        this.singleItemPrice = singleItemPrice;
-    }
-
-    public Double getLineItemTotalPrice() {
-        return lineItemTotalPrice;
-    }
-
-    public void setLineItemTotalPrice(Double lineItemTotalPrice) {
-        this.lineItemTotalPrice = lineItemTotalPrice;
-    }
-
-    public Boolean getShipped() {
-        return shipped;
-    }
-
-    public void setShipped(Boolean shipped) {
-        this.shipped = shipped;
-    }
-
-    public Boolean getDelivered() {
-        return delivered;
-    }
-
-    public void setDelivered(Boolean delivered) {
-        this.delivered = delivered;
-    }
 }
