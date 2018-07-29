8877cc6251268b07f33b4dc5cb1085e8d5b98312
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-29 12:47:19 -0500

    updated files and added REST template

--- applications/orders/src/main/java/com/microservices/orders/lineItems/LineItemController.java
+++ applications/orders/src/main/java/com/microservices/orders/lineItems/LineItemController.java
@@ -1,58 +1,8 @@
 package com.microservices.orders.lineItems;
 
-import com.netflix.discovery.converters.Auto;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping(value = {"/lineItems"})
 public class LineItemController {
-
-    @Autowired
-    private LineItemRepository lineItemRepository;
-
-    @GetMapping
-    public List<LineItem> showAllLineItems(){
-        return lineItemRepository.findAll();
-    }
-
-    @GetMapping(value = {"/{lineItemId}"})
-    public List<LineItem> showIndividualLineItem(
-            @PathVariable(name = "lineItemId") Long lineItemId
-    ){
-        return lineItemRepository.findAllById(lineItemId);
-    }
-
-    @PostMapping
-    public ResponseEntity newLineItem(
-            @RequestBody LineItem lineItem
-    ){
-        lineItem.setQuantity(lineItem.getQuantity());
-        lineItem.setLineItemTotalPrice(lineItem.getLineItemTotalPrice());
-        lineItem.setOrderId(lineItem.getOrderId());
-        lineItem.setProductId(lineItem.getProductId());
-        lineItem.setShipmentId(lineItem.getShipmentId());
-        lineItem.setSingleItemPrice(lineItem.getSingleItemPrice());
-
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @PutMapping
-    public ResponseEntity updateLineItem(){
-
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @DeleteMapping
-    public ResponseEntity deleteLineItem(){
-
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-
 
 }
