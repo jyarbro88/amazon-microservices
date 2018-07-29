10001924eaaaf407ef91c5ae309a026162b8e6e8
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-29 12:45:33 -0500

    added Lombok back

--- applications/accounts/src/main/java/com/microservices/accounts/accounts/Account.java
+++ applications/accounts/src/main/java/com/microservices/accounts/accounts/Account.java
@@ -1,11 +1,15 @@
 package com.microservices.accounts.accounts;
 
 import com.microservices.accounts.addresses.Address;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Getter @Setter @NoArgsConstructor
 @Table(name = "accounts")
 public class Account {
 
@@ -18,54 +22,10 @@ public class Account {
     @OneToMany
     private List<Address> addressIds;
 
-    public Account(){
-
-    }
-
     public Account(String firstName, String lastName, String email, List<Address> addressIds) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.addressIds = addressIds;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Address> getAddressIds() {
-        return addressIds;
-    }
-
-    public void setAddressIds(List<Address> addressIds) {
         this.addressIds = addressIds;
     }
 }
