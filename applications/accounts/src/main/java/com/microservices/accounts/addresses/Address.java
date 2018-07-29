83a5cfd9f3c668f733baec54c9ff2065d32bbbcc
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-29 12:46:35 -0500

    added Lomback back to model

--- applications/accounts/src/main/java/com/microservices/accounts/addresses/Address.java
+++ applications/accounts/src/main/java/com/microservices/accounts/addresses/Address.java
@@ -1,6 +1,8 @@
 package com.microservices.accounts.addresses;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Getter @Setter @NoArgsConstructor
 @Table(name = "addresses")
 public class Address {
 
@@ -21,71 +24,12 @@ public class Address {
     private String zipCode;
     private String country;
 
-    public Address() {
-    }
-
     public Address(String addressOne, String addressTwo, String city, String state, String zipCode, String country) {
         this.addressOne = addressOne;
         this.addressTwo = addressTwo;
         this.city = city;
         this.state = state;
         this.zipCode = zipCode;
-        this.country = country;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAddressOne() {
-        return addressOne;
-    }
-
-    public void setAddressOne(String addressOne) {
-        this.addressOne = addressOne;
-    }
-
-    public String getAddressTwo() {
-        return addressTwo;
-    }
-
-    public void setAddressTwo(String addressTwo) {
-        this.addressTwo = addressTwo;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
         this.country = country;
     }
 }
