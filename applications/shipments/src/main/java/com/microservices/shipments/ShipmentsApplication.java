2f935a3202364b9be57e1871977d101e03349e82
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-29 12:48:00 -0500

    added REST Template

--- applications/shipments/src/main/java/com/microservices/shipments/ShipmentsApplication.java
+++ applications/shipments/src/main/java/com/microservices/shipments/ShipmentsApplication.java
@@ -2,11 +2,22 @@ package com.microservices.shipments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ShipmentsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShipmentsApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
