dc41dbe4e0a9cf3b958839ae09ed3661c330e0e2
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-30 12:04:41 -0500

    made an Order Address model to display

---
+++ applications/orders/src/main/java/com/microservices/orders/tempModels/OrderAddressModel.java
@@ -0,0 +1,17 @@
+package com.microservices.orders.tempModels;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor
+public class OrderAddressModel {
+
+    private Long id;
+    private String addressOne;
+    private String addressTwo;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+}
