3b82ba8c338e9344d7a18676b490914dc6fcde1f
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-30 12:04:04 -0500

    added micro services class to reference all eureka instance names

---
+++ applications/orders/src/main/java/com/microservices/orders/MicroServiceInstances.java
@@ -0,0 +1,18 @@
+package com.microservices.orders;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MicroServiceInstances {
+
+
+    public String getAllShipmentsForOrderId(Long orderId){
+        return "http://shipments-service/shipments/" + orderId;
+    }
+
+    public String getOrderShippingAddress(Long accountIdToSearch, Long addressIdToSearch){
+
+        return "http://accounts-service/accounts/" + accountIdToSearch + "/addresses/" + addressIdToSearch;
+    }
+
+}
